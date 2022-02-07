7774441907e517a3bc40a2470efc59583b98daca
Smita1706 <smitachinchkar1998@gmail.com>
2022-02-07 16:56:40 +0530

    Added : Use case 11 use interface approach to manage wage of companies is added

---
+++ WageBuilder_UC11.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.WageBuilderDay10;
+
+public class WageBuilder_UC11 implements ComputeWageInterface{
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	private int numOfCompany = 0;
+	private  CompanyEmpWage[] arr = new CompanyEmpWage[5];
+	
+	public void addCompanyEmpWage(String com, int wage, int days,int hrs) {
+		arr[numOfCompany] = new  CompanyEmpWage( com, wage,days,hrs);
+		numOfCompany++;
+	}
+	public void computeEmpWage() {
+		int i;
+		for(i = 0 ; i < numOfCompany ; i++) {
+			arr[i].setTotalSalary(this.wgeComputeForCompany(arr[i]));
+			System.out.println(arr[i]);
+		}
+	}
+	public int wgeComputeForCompany(CompanyEmpWage a) {
+		int workingHour = 0;
+		int totalWorkingHour = 0;
+		int salary = 0;
+		int totalSalary = 0;
+		int i = 0;
+		while(i < a.noOfDays &&  totalWorkingHour <= a.maxHourInMonth) {
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			int Check = (int)empCheck;
+			switch(Check) {
+			case 1:
+				workingHour = 8;
+				break;
+			case 2 :
+				workingHour = 4;
+				break;
+			default : 
+				workingHour = 0;
+				break;
+			}
+			i++;
+			salary = a.wagePerHour *  workingHour;
+			totalSalary += salary;
+			totalWorkingHour +=  workingHour;
+		}
+		System.out.println("total working hours are  : " + totalWorkingHour);
+		return totalSalary;
+	}
+	
+	public static void main(String[] args) {
+		ComputeWageInterface a = new WageBuilder_UC11();
+		a.addCompanyEmpWage("D-mart", 20, 2, 10);
+		a.addCompanyEmpWage("k-mart", 10, 2, 10);
+		a.computeEmpWage();		
+	}
+}
